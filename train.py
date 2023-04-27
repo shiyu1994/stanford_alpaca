9598808525c105653f2e37a96117a0373b01af07
shiyu1994 <shiyu_k1994@qq.com>
2023-04-27 21:02:52 +0800

    add lora

--- train.py
+++ train.py
@@ -23,6 +23,14 @@ import utils
 from torch.utils.data import Dataset
 from transformers import Trainer
 
+import os
+
+from peft import LoraConfig, get_peft_model, prepare_model_for_int8_training
+
+import json
+
+from icecream import ic
+
 IGNORE_INDEX = -100
 DEFAULT_PAD_TOKEN = "[PAD]"
 DEFAULT_EOS_TOKEN = "</s>"
@@ -59,6 +67,14 @@ class TrainingArguments(transformers.TrainingArguments):
     model_max_length: int = field(
         default=512,
         metadata={"help": "Maximum sequence length. Sequences will be right padded (and possibly truncated)."},
+    )
+    use_lora: bool = field(
+        default=False,
+        metadata={"help": "whether to use LoRA in finetuning"},
+    )
+    use_8bit: bool = field(
+        default=False,
+        metadata={"help": "whether to use 8-bit training"},
     )
 
 
@@ -179,6 +195,33 @@ def make_supervised_data_module(tokenizer: transformers.PreTrainedTokenizer, dat
     return dict(train_dataset=train_dataset, eval_dataset=None, data_collator=data_collator)
 
 
+def create_peft_model(model):
+    LORA_R = 8
+    LORA_ALPHA = 16
+    LORA_DROPOUT = 0.05
+    TARGET_MODULES = [
+        "q_proj",
+        "k_proj",
+        "v_proj",
+        "down_proj",
+        "gate_proj",
+        "up_proj",
+    ]
+    
+    lora_config = LoraConfig(
+        r=LORA_R,
+        lora_alpha=LORA_ALPHA,
+        target_modules=TARGET_MODULES,
+        lora_dropout=LORA_DROPOUT,
+        bias="none",
+        task_type="CAUSAL_LM",
+    )
+
+    model = get_peft_model(model, lora_config)
+
+    return model
+
+
 def train():
     parser = transformers.HfArgumentParser((ModelArguments, DataArguments, TrainingArguments))
     model_args, data_args, training_args = parser.parse_args_into_dataclasses()
@@ -186,6 +229,8 @@ def train():
     model = transformers.AutoModelForCausalLM.from_pretrained(
         model_args.model_name_or_path,
         cache_dir=training_args.cache_dir,
+        load_in_8bit=training_args.use_8bit,
+        device_map={"": int(os.environ.get("LOCAL_RANK") or 0)} if training_args.use_8bit else None,
     )
 
     tokenizer = transformers.AutoTokenizer.from_pretrained(
@@ -211,7 +256,21 @@ def train():
         model=model,
     )
 
+    if training_args.use_8bit:
+        model = prepare_model_for_int8_training(model, use_gradient_checkpointing=training_args.gradient_checkpointing)
+
+    if training_args.use_lora:
+        model = create_peft_model(model)
+
     data_module = make_supervised_data_module(tokenizer=tokenizer, data_args=data_args)
+
+    if training_args.use_8bit:
+        with open(training_args.deepspeed, "r") as in_file:
+            deepspeed_config = json.load(in_file)
+        deepspeed_config["zero_optimization"]["stage"] = 2
+        with open(training_args.deepspeed, "w") as out_file:
+            json.dump(deepspeed_config, out_file)
+
     trainer = Trainer(model=model, tokenizer=tokenizer, args=training_args, **data_module)
     trainer.train()
     trainer.save_state()
